fd413056bca5dea1ad9aceeaf677e6ab6833d9fc
Ana Paiz Rivera <54344320+AnaPaiz@users.noreply.github.com>
2023-02-06 18:34:52 -0600

    Update UI.js

--- UI.js
+++ UI.js
@@ -7,30 +7,59 @@ const onLoadHome = () => {
   showInformation.showInfo();
 };
 /*Cambiar pestañas */
-const clickOpc = document.querySelectorAll(".changeOptions");
+const clickOpc = document.querySelectorAll(".option");
 clickOpc.forEach((tab) => {
   tab.addEventListener("click", function () {
     let sectId = this.id;
     const section = document.querySelectorAll('.section');
     const element = document.querySelector("."+ sectId);
-    
+    const btnColor = document.querySelectorAll(".btnColor");
+    const active = document.getElementById(sectId);
+   /*Para hacer cambio de color de boton */
+    btnColor.forEach(btn =>{
+      if(btn.classList.contains("active")){
+        btn.classList.remove("active");
+      }
+    })
+    active.classList.add("active");
+    /*Para hacer cambio de contenido */
     section.forEach(
         btn =>{
-            if(btn.classList.contains("showPage")){
-                btn.classList.remove("showPage");
-                btn.classList.add("hiddenPage")
+            if(!btn.classList.contains("hidden")){
+                btn.classList.add("hidden");
             }
-        }
-    )
-        element.classList.remove("hiddenPage");
-        element.classList.add("showPage");
+          })   
+        element.classList.remove("hidden");
   });
 });
 
-const sendMessage = (e) =>{
-                            
-  alert("Este es un mensaje para comprobar si funciona la función")
+const form = document.getElementById('myForm');
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const txtName = e.target.nameText.value;
+  const textLastName = e.target.lastNameText.value;
+  const txtEmail= e.target.emailText.value;
+  const txtAffair = e.target.affairText.value;
+  const txtMessage = document.getElementById("messageText").value;
+  
+  if(!txtName || !textLastName || !txtEmail || !txtAffair || !txtMessage){
+    showMessage("Hay un error con los campos, por favor revise", "error");
+  } else{
+    showMessage("Envío de datos exitoso", "success");
+  }
+});
+
+const showMessage = (message, type)=>{
+  const msgContainer = document.querySelector(".us__message");
+  const addInfo = document.createElement("div");
+  addInfo.innerHTML=`<p>${message}</p>`;
+  msgContainer.classList.add(`${type}`);
+  msgContainer.append(addInfo);
+  setTimeout(()=>{
+    msgContainer.remove();
+  }, 3000)
 }
 
 /*Cambiar páginas*/
 window.onload = onLoadHome;
+ /**/ 
